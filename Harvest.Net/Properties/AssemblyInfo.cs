c34ee3c10b6845d408605b01c66bbaf6d9a033c3
ithielnor <ithielnor@gmail.com>
2014-06-10 19:24:17 -0400

    Added start nuspec. Added blank Invoices resource.

--- Harvest.Net/Properties/AssemblyInfo.cs
+++ Harvest.Net/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Harvest.Net")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("An API wrapper for Harvest time tracking.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Joel Potter, IthiEnterprises")]
 [assembly: AssemblyProduct("Harvest.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]

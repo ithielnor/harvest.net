f1a39e2909dd346b2dd3ed5ac466820c61d8121c
ithielnor <ithielnor@gmail.com>
2014-06-13 10:51:48 -0400

    Corrected assembly details. Version bump.

--- Harvest.Net/Properties/AssemblyInfo.cs
+++ Harvest.Net/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Harvest.Net")]
 [assembly: AssemblyDescription("An API wrapper for Harvest time tracking.")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Joel Potter, IthiEnterprises")]
+[assembly: AssemblyCompany("Joel Potter, Data Research Group")]
 [assembly: AssemblyProduct("Harvest.Net")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014, Joel Potter and Data Research Group")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]

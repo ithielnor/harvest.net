92a5c2ee4a625ccdeaf7f3703a0e5ad8ba276b19
Caleb Simon <caleb.simon@gmail.com>
2014-12-15 09:52:42 -0700

    Time and Expense Reporting
    Proposed added support

---
+++ Harvest.Net/Models/DayEntry.cs
@@ -0,0 +1,40 @@
+﻿using RestSharp.Serializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Harvest.Net.Models
+{
+    [SerializeAs(Name = "dayentry")]
+    public class DayEntry : Harvest.Net.Models.IModel
+    {
+        public long Id { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public string Notes { get; set; }
+
+        public DateTime SpentAt { get; set; }
+
+        public double Hours { get; set; }
+
+        public int UserId { get; set; }
+
+        public int ProjectId { get; set; }
+
+        public int TaskId { get; set; }
+
+        public bool AdjecustmentRecord { get; set; }
+
+        //public DateTime TimerStartedAt { get; set; }
+
+        public bool IsBilled { get; set; }
+
+        public bool IsClosed { get; set; }
+
+    }
+}

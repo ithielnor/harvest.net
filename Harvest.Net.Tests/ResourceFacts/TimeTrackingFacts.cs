46ec9c11830b8f45441e887e691790f95a6159a0
ithielnor <ithielnor@gmail.com>
2015-02-11 22:36:16 -0500

    Unfinished time tracking tests

--- Harvest.Net.Tests/ResourceFacts/TimeTrackingFacts.cs
+++ Harvest.Net.Tests/ResourceFacts/TimeTrackingFacts.cs
@@ -7,6 +7,8 @@ namespace Harvest.Net.Tests
 {
     public class TimeTrackingFacts : FactBase, IDisposable
     {
+        DayEntry _todelete = null;
+
         [Fact]
         public void Daily_ReturnsResult()
         {
@@ -17,10 +19,32 @@ namespace Harvest.Net.Tests
             Assert.NotNull(result.Projects);
             Assert.Equal(DateTime.Now.Date, result.ForDay);
         }
+
+        [Fact]
+        public void Daily_ReturnsCorrectResult()
+        {
+            var result = Api.Daily(286857409);
+
+            Assert.NotNull(result);
+            Assert.Equal(1m, result.DayEntry.Hours);
+            Assert.Equal(new DateTime(2014, 12, 19), result.DayEntry.SpentAt);
+        }
+
+        [Fact]
+        public void CreateDaily_CreatesAnEntry()
+        {
+            var result = Api.CreateDaily(DateTime.Now.Date, GetTestId(TestId.ProjectId), GetTestId(TestId.TaskId), 2);
+            _todelete = result.DayEntry;
+
+            Assert.Equal(2m, result.DayEntry.Hours);
+            Assert.Equal(GetTestId(TestId.ProjectId), result.DayEntry.ProjectId);
+            Assert.Equal(GetTestId(TestId.TaskId), result.DayEntry.TaskId);
+        }
         
         public void Dispose()
         {
-            
+            if (_todelete != null)
+                Api.DeleteDaily(_todelete.Id);
         }
     }
 }

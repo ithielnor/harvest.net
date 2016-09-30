52488e0dccc038ebc41b09801d49424aff0be5f0
igeligel <kevinigeligeligel@gmail.com>
2016-10-01 00:24:28 +0200

    Revert "Add documentation for contact and contactoptions models #39"
    This reverts commit c3885768315652e73be6fea635be97d2c692c436.

--- Harvest.Net/Models/ContactOptions.cs
+++ Harvest.Net/Models/ContactOptions.cs
@@ -5,51 +5,20 @@ namespace Harvest.Net.Models
     [SerializeAs(Name = "contact")]
     public class ContactOptions
     {
-        /// <summary>
-        /// Identifier of the client.
-        /// <example>
-        /// 3398386
-        /// </example>
-        /// </summary>
         public long? ClientId { get; set; }
 
-        /// <summary>
-        /// First name of the contact.
-        /// </summary>
         public string FirstName { get; set; }
 
-        /// <summary>
-        /// Last name of the contact.
-        /// </summary>
         public string LastName { get; set; }
 
-        /// <summary>
-        /// Title of the contact.
-        /// Example: 
-        /// <example>
-        /// Mrs
-        /// </example>
-        /// </summary>
         public string Title { get; set; }
 
-        /// <summary>
-        /// E-Mail of the contact.
-        /// </summary>
         public string Email { get; set; }
 
-        /// <summary>
-        /// Phone number to the office of the contact.
-        /// </summary>
         public string PhoneOffice { get; set; }
 
-        /// <summary>
-        /// Phone number of the mobile phone of the contact.
-        /// </summary>
         public string PhoneMobile { get; set; }
 
-        /// <summary>
-        /// Fax of the contact.
-        /// </summary>
         public string Fax { get; set; }
     }
 }

bfc21d86ed3e353168f26bd650718e7050a2bb8a
igeligel <kevinigeligeligel@gmail.com>
2016-10-01 00:26:13 +0200

    Add full documentation for contact and contact options model #39

--- Harvest.Net/Models/Contact.cs
+++ Harvest.Net/Models/Contact.cs
@@ -3,29 +3,77 @@ using System;
 
 namespace Harvest.Net.Models
 {
+    /// <summary>
+    /// Model for a contact in the harvest system.
+    /// It is used to add or update contacts.
+    /// </summary>
     [SerializeAs(Name = "contact")]
     public class Contact : IModel
     {
+        /// <summary>
+        /// Identifier of the contact.
+        /// Example:
+        /// <example>
+        /// 2937808
+        /// </example>
+        /// </summary>
         public long Id { get; set; }
 
+        /// <summary>
+        /// Time when the contact entry was updated.
+        /// </summary>
         public DateTime UpdatedAt { get; set; }
 
+        /// <summary>
+        /// Time when the contact entry was created.
+        /// </summary>
         public DateTime CreatedAt { get; set; }
 
+        /// <summary>
+        /// Identifier of the client.
+        /// <example>
+        /// 3398386
+        /// </example>
+        /// </summary>
         public long ClientId { get; set; }
 
+        /// <summary>
+        /// First name of the contact.
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Last name of the contact.
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Title of the contact.
+        /// Example: 
+        /// <example>
+        /// Mrs
+        /// </example>
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// E-Mail of the contact.
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// Phone number to the office of the contact.
+        /// </summary>
         public string PhoneOffice { get; set; }
 
+        /// <summary>
+        /// Phone number of the mobile phone of the contact.
+        /// </summary>
         public string PhoneMobile { get; set; }
 
+        /// <summary>
+        /// Fax of the contact.
+        /// </summary>
         public string Fax { get; set; }
     }
 }
--- Harvest.Net/Models/ContactOptions.cs
+++ Harvest.Net/Models/ContactOptions.cs
@@ -2,23 +2,57 @@ using RestSharp.Serializers;
 
 namespace Harvest.Net.Models
 {
+    /// <summary>
+    /// Options which are used for interacting with contacts in the harvest API.
+    /// </summary>
     [SerializeAs(Name = "contact")]
     public class ContactOptions
     {
+        /// <summary>
+        /// Identifier of the client.
+        /// <example>
+        /// 3398386
+        /// </example>
+        /// </summary>
         public long? ClientId { get; set; }
 
+        /// <summary>
+        /// First name of the contact.
+        /// </summary>
         public string FirstName { get; set; }
 
+        /// <summary>
+        /// Last name of the contact.
+        /// </summary>
         public string LastName { get; set; }
 
+        /// <summary>
+        /// Title of the contact.
+        /// Example: 
+        /// <example>
+        /// Mrs
+        /// </example>
+        /// </summary>
         public string Title { get; set; }
 
+        /// <summary>
+        /// E-Mail of the contact.
+        /// </summary>
         public string Email { get; set; }
 
+        /// <summary>
+        /// Phone number to the office of the contact.
+        /// </summary>
         public string PhoneOffice { get; set; }
 
+        /// <summary>
+        /// Phone number of the mobile phone of the contact.
+        /// </summary>
         public string PhoneMobile { get; set; }
 
+        /// <summary>
+        /// Fax of the contact.
+        /// </summary>
         public string Fax { get; set; }
     }
 }

09b808011f7811b0765cb46266e7573049768f8d
Paul Irwin <paulirwin@gmail.com>
2017-09-28 11:18:18 -0400

    Add Tasks async methods

--- Harvest.Net/Resources/Tasks.cs
+++ Harvest.Net/Resources/Tasks.cs
@@ -1,8 +1,8 @@
 ﻿using Harvest.Net.Models;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
 
 namespace Harvest.Net
 {
@@ -14,25 +14,45 @@ namespace Harvest.Net
         /// List all tasks for the authenticated account. Makes a GET request to the Tasks resource.
         /// </summary>
         /// <param name="updatedSince">An optional filter on the task updated-at property</param>
-        public IList<Task> ListTasks(DateTime? updatedSince = null)
+        public IList<Models.Task> ListTasks(DateTime? updatedSince = null)
         {
-            var request = Request("tasks");
+            var request = ListTasksRequest(updatedSince);
 
-            if (updatedSince != null)
-                request.AddParameter("updated_since", updatedSince.Value.ToString("yyyy-MM-dd HH:mm"));
+            return Execute<List<Models.Task>>(request);
+        }
 
-            return Execute<List<Task>>(request);
+        /// <summary>
+        /// List all tasks for the authenticated account. Makes a GET request to the Tasks resource.
+        /// </summary>
+        /// <param name="updatedSince">An optional filter on the task updated-at property</param>
+        public async Task<IList<Models.Task>> ListTasksAsync(DateTime? updatedSince = null)
+        {
+            var request = ListTasksRequest(updatedSince);
+
+            // ReSharper disable once AsyncConverter.AsyncAwaitMayBeElidedHighlighting
+            return await ExecuteAsync<List<Models.Task>>(request).ConfigureAwait(false);
         }
 
         /// <summary>
         /// Retrieve a task on the authenticated account. Makes a GET request to the Tasks resource.
         /// </summary>
         /// <param name="taskId">The Id of the task to retrieve</param>
-        public Task Task(long taskId)
+        public Models.Task Task(long taskId)
         {
             var request = Request("tasks/" + taskId);
 
-            return Execute<Task>(request);
+            return Execute<Models.Task>(request);
+        }
+
+        /// <summary>
+        /// Retrieve a task on the authenticated account. Makes a GET request to the Tasks resource.
+        /// </summary>
+        /// <param name="taskId">The Id of the task to retrieve</param>
+        public Task<Models.Task> TaskAsync(long taskId)
+        {
+            var request = Request("tasks/" + taskId);
+
+            return ExecuteAsync<Models.Task>(request);
         }
 
         /// <summary>
@@ -42,33 +62,51 @@ namespace Harvest.Net
         /// <param name="billableByDefault">Whether the task should be billable when added to a project</param>
         /// <param name="isDefault">Whether the task should be added to new projects</param>
         /// <param name="defaultHourlyRate">The default hourly rate</param>
-        public Task CreateTask(string name, bool billableByDefault = false, bool isDefault = false, decimal? defaultHourlyRate = null)
+        public Models.Task CreateTask(string name, bool billableByDefault = false, bool isDefault = false, decimal? defaultHourlyRate = null)
         {
-            if (name == null)
-                throw new ArgumentNullException("name");
-
-            var options = new TaskOptions()
-            {
-                Name = name,
-                BillableByDefault = billableByDefault,
-                IsDefault = isDefault,
-                DefaultHourlyRate = defaultHourlyRate
-            };
+            var options = GetTaskOptions(name, billableByDefault, isDefault, defaultHourlyRate);
 
             return CreateTask(options);
+        }
+
+        /// <summary>
+        /// Creates a new task under the authenticated account. Makes both a POST and a GET request to the Tasks resource.
+        /// </summary>
+        /// <param name="name">The name of the task</param>
+        /// <param name="billableByDefault">Whether the task should be billable when added to a project</param>
+        /// <param name="isDefault">Whether the task should be added to new projects</param>
+        /// <param name="defaultHourlyRate">The default hourly rate</param>
+        public Task<Models.Task> CreateTaskAsync(string name, bool billableByDefault = false, bool isDefault = false, decimal? defaultHourlyRate = null)
+        {
+            var options = GetTaskOptions(name, billableByDefault, isDefault, defaultHourlyRate);
+
+            return CreateTaskAsync(options);
         }
 
         /// <summary>
         /// Creates a new task under the authenticated account. Makes a POST and a GET request to the Tasks resource.
         /// </summary>
         /// <param name="options">The options for the new task to be created</param>
-        public Task CreateTask(TaskOptions options)
+        public Models.Task CreateTask(TaskOptions options)
         {
-            var request = Request("tasks", RestSharp.Method.POST);
+            var request = Request("tasks", Method.POST);
 
             request.AddBody(options);
 
-            return Execute<Task>(request);
+            return Execute<Models.Task>(request);
+        }
+
+        /// <summary>
+        /// Creates a new task under the authenticated account. Makes a POST and a GET request to the Tasks resource.
+        /// </summary>
+        /// <param name="options">The options for the new task to be created</param>
+        public Task<Models.Task> CreateTaskAsync(TaskOptions options)
+        {
+            var request = Request("tasks", Method.POST);
+
+            request.AddBody(options);
+
+            return ExecuteAsync<Models.Task>(request);
         }
 
         /// <summary>
@@ -77,9 +115,22 @@ namespace Harvest.Net
         /// <param name="taskId">The ID of the task to delete</param>
         public bool DeleteTask(long taskId)
         {
-            var request = Request("tasks/" + taskId, RestSharp.Method.DELETE);
+            var request = Request("tasks/" + taskId, Method.DELETE);
 
             var result = Execute(request);
+
+            return result.StatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        /// <summary>
+        /// Delete a task from the authenticated account. Makes a DELETE request to the Tasks resource.
+        /// </summary>
+        /// <param name="taskId">The ID of the task to delete</param>
+        public async Task<bool> DeleteTaskAsync(long taskId)
+        {
+            var request = Request("tasks/" + taskId, Method.DELETE);
+
+            var result = await ExecuteAsync(request).ConfigureAwait(false);
 
             return result.StatusCode == System.Net.HttpStatusCode.OK;
         }
@@ -88,31 +139,52 @@ namespace Harvest.Net
         /// Activate a task on the authenticated account. Makes a POST request to the Tasks/Activate resource and a GET request to the Tasks resource.
         /// </summary>
         /// <param name="taskId">The ID of the task to activate</param>
-        public Task ActivateTask(long taskId)
+        public Models.Task ActivateTask(long taskId)
         {
-            var request = Request("tasks/" + taskId + "/activate", RestSharp.Method.POST);
+            var request = Request("tasks/" + taskId + "/activate", Method.POST);
 
-            return Execute<Task>(request);
+            return Execute<Models.Task>(request);
+        }
+
+        /// <summary>
+        /// Activate a task on the authenticated account. Makes a POST request to the Tasks/Activate resource and a GET request to the Tasks resource.
+        /// </summary>
+        /// <param name="taskId">The ID of the task to activate</param>
+        public Task<Models.Task> ActivateTaskAsync(long taskId)
+        {
+            var request = Request("tasks/" + taskId + "/activate", Method.POST);
+
+            return ExecuteAsync<Models.Task>(request);
         }
 
         /// <summary>
         /// Update a task on the authenticated account. Makes a PUT and a GET request to the Tasks resource.
         /// </summary>
+        /// <param name="taskId">The Id of the task</param>
         /// <param name="name">The name of the task</param>
         /// <param name="billableByDefault">Whether the task should be billable when added to a project</param>
         /// <param name="isDefault">Whether the task should be added to new projects</param>
         /// <param name="defaultHourlyRate">The default hourly rate</param>
-        public Task UpdateTask(long taskId, string name = null, bool? billableByDefault = null, bool? isDefault = null, decimal? defaultHourlyRate = null)
+        public Models.Task UpdateTask(long taskId, string name = null, bool? billableByDefault = null, bool? isDefault = null, decimal? defaultHourlyRate = null)
         {
-            var options = new TaskOptions()
-            {
-                Name = name,
-                BillableByDefault = billableByDefault,
-                IsDefault = isDefault,
-                DefaultHourlyRate = defaultHourlyRate
-            };
+            var options = GetTaskOptions(name, billableByDefault, isDefault, defaultHourlyRate);
 
             return UpdateTask(taskId, options);
+        }
+
+        /// <summary>
+        /// Update a task on the authenticated account. Makes a PUT and a GET request to the Tasks resource.
+        /// </summary>
+        /// <param name="taskId">The Id of the task</param>
+        /// <param name="name">The name of the task</param>
+        /// <param name="billableByDefault">Whether the task should be billable when added to a project</param>
+        /// <param name="isDefault">Whether the task should be added to new projects</param>
+        /// <param name="defaultHourlyRate">The default hourly rate</param>
+        public Task<Models.Task> UpdateTaskAsync(long taskId, string name = null, bool? billableByDefault = null, bool? isDefault = null, decimal? defaultHourlyRate = null)
+        {
+            var options = GetTaskOptions(name, billableByDefault, isDefault, defaultHourlyRate);
+
+            return UpdateTaskAsync(taskId, options);
         }
 
         /// <summary>
@@ -120,13 +192,53 @@ namespace Harvest.Net
         /// </summary>
         /// <param name="taskId">The ID for the task to update</param>
         /// <param name="options">The options to be updated</param>
-        public Task UpdateTask(long taskId, TaskOptions options)
+        public Models.Task UpdateTask(long taskId, TaskOptions options)
         {
-            var request = Request("tasks/" + taskId, RestSharp.Method.PUT);
+            var request = Request("tasks/" + taskId, Method.PUT);
 
             request.AddBody(options);
 
-            return Execute<Task>(request);
+            return Execute<Models.Task>(request);
+        }
+
+        /// <summary>
+        /// Updates a task on the authenticated account. Makes a PUT and a GET request to the Tasks resource.
+        /// </summary>
+        /// <param name="taskId">The ID for the task to update</param>
+        /// <param name="options">The options to be updated</param>
+        public Task<Models.Task> UpdateTaskAsync(long taskId, TaskOptions options)
+        {
+            var request = Request("tasks/" + taskId, Method.PUT);
+
+            request.AddBody(options);
+
+            return ExecuteAsync<Models.Task>(request);
+        }
+
+        private IRestRequest ListTasksRequest(DateTime? updatedSince)
+        {
+            var request = Request("tasks");
+
+            if (updatedSince != null)
+                request.AddParameter("updated_since", updatedSince.Value.ToString("yyyy-MM-dd HH:mm"));
+
+            return request;
+        }
+
+        private static TaskOptions GetTaskOptions(string name, bool? billableByDefault, bool? isDefault, decimal? defaultHourlyRate)
+        {
+            if (name == null)
+                throw new ArgumentNullException(nameof(name));
+
+            var options = new TaskOptions
+            {
+                Name = name,
+                BillableByDefault = billableByDefault,
+                IsDefault = isDefault,
+                DefaultHourlyRate = defaultHourlyRate
+            };
+
+            return options;
         }
     }
 }

1c633664b18f4802674ba2f0d4ceee92fbfa960a
Paul Irwin <paulirwin@gmail.com>
2017-09-28 11:39:53 -0400

    Add TimeTracking async methods

--- Harvest.Net/ResourceInterfaces/TimeTracking.cs
+++ Harvest.Net/ResourceInterfaces/TimeTracking.cs
@@ -1,8 +1,5 @@
 ﻿using Harvest.Net.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Harvest.Net
@@ -11,18 +8,34 @@ namespace Harvest.Net
     {
         Daily Daily(short? dayOfTheYear = null, short? year = null, long? ofUser = null);
 
+        Task<Daily> DailyAsync(short? dayOfTheYear = null, short? year = null, long? ofUser = null);
+
         Timer Daily(long dayEntryId, long? ofUser = null);
-        
+
+        Task<Timer> DailyAsync(long dayEntryId, long? ofUser = null);
+
         Timer ToggleDaily(long dayEntryId, long? ofUser = null);
+
+        Task<Timer> ToggleDailyAsync(long dayEntryId, long? ofUser = null);
 
         Timer CreateDaily(DateTime spentAt, long projectId, long taskId, decimal hours, string notes = null, long? ofUser = null);
 
+        Task<Timer> CreateDailyAsync(DateTime spentAt, long projectId, long taskId, decimal hours, string notes = null, long? ofUser = null);
+
         Timer CreateDaily(DateTime spentAt, long projectId, long taskId, TimeSpan startedAt, TimeSpan endedAt, string notes = null, long? ofUser = null);
 
+        Task<Timer> CreateDailyAsync(DateTime spentAt, long projectId, long taskId, TimeSpan startedAt, TimeSpan endedAt, string notes = null, long? ofUser = null);
+
         Timer StartTimer(DateTime spentAt, long projectId, long taskId, string notes = null, long? ofUser = null);
-        
+
+        Task<Timer> StartTimerAsync(DateTime spentAt, long projectId, long taskId, string notes = null, long? ofUser = null);
+
         bool DeleteDaily(long dayEntryId, long? ofUser = null);
 
+        Task<bool> DeleteDailyAsync(long dayEntryId, long? ofUser = null);
+
         Timer UpdateDaily(long dayEntryId, DateTime? spentAt = null, long? projectId = null, long? taskId = null, decimal? hours = null, TimeSpan? startedAt = null, TimeSpan? endedAt = null, string notes = null, long? ofUser = null);
+
+        Task<Timer> UpdateDailyAsync(long dayEntryId, DateTime? spentAt = null, long? projectId = null, long? taskId = null, decimal? hours = null, TimeSpan? startedAt = null, TimeSpan? endedAt = null, string notes = null, long? ofUser = null);
     }
 }
--- Harvest.Net/Resources/TimeTracking.cs
+++ Harvest.Net/Resources/TimeTracking.cs
@@ -1,8 +1,7 @@
 using Harvest.Net.Models;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
 
 namespace Harvest.Net
 {
@@ -18,17 +17,22 @@ namespace Harvest.Net
         /// <param name="ofUser">The user the day entry belongs to</param>
         public Daily Daily(short? dayOfTheYear = null, short? year = null, long? ofUser = null)
         {
-            if (year == null && dayOfTheYear != null)
-                throw new ArgumentNullException("year", "You must provide both dayOfTheYear and year or neither.");
-            if (year != null && dayOfTheYear == null)
-                throw new ArgumentNullException("dayOfTheYear", "You must provide both dayOfYear and year or neither.");
-
-            var request = Request("daily" + (dayOfTheYear != null ? "/" + dayOfTheYear + "/" + year : string.Empty));
-
-            if (ofUser != null)
-                request.AddParameter("of_user", ofUser.Value);
+            var request = DailyRequest(dayOfTheYear, year, ofUser);
 
             return Execute<Daily>(request);
+        }
+
+        /// <summary>
+        /// List all time entries logged by the current user for a given day of the year. Makes a GET request to the Daily resource.
+        /// </summary>
+        /// <param name="dayOfTheYear">The day of the year to list (1-366). If null, lists today.</param>
+        /// <param name="year">The year to list. If null, lists today.</param>
+        /// <param name="ofUser">The user the day entry belongs to</param>
+        public Task<Daily> DailyAsync(short? dayOfTheYear = null, short? year = null, long? ofUser = null)
+        {
+            var request = DailyRequest(dayOfTheYear, year, ofUser);
+
+            return ExecuteAsync<Daily>(request);
         }
 
         /// <summary>
@@ -38,12 +42,21 @@ namespace Harvest.Net
         /// <param name="ofUser">The user the day entry belongs to</param>
         public Timer Daily(long dayEntryId, long? ofUser = null)
         {
-            var request = Request("daily/show/" + dayEntryId);
-
-            if (ofUser != null)
-                request.AddParameter("of_user", ofUser.Value);
+            var request = DailyRequest(dayEntryId, ofUser);
 
             return Execute<Timer>(request);
+        }
+
+        /// <summary>
+        /// Retrieve a single day entry by ID. Makes a GET request to the Daily/Show resource.
+        /// </summary>
+        /// <param name="dayEntryId">The ID of the day entry to retrieve</param>
+        /// <param name="ofUser">The user the day entry belongs to</param>
+        public Task<Timer> DailyAsync(long dayEntryId, long? ofUser = null)
+        {
+            var request = DailyRequest(dayEntryId, ofUser);
+
+            return ExecuteAsync<Timer>(request);
         }
 
         /// <summary>
@@ -53,12 +66,21 @@ namespace Harvest.Net
         /// <param name="ofUser">The user the day entry belongs to</param>
         public Timer ToggleDaily(long dayEntryId, long? ofUser = null)
         {
-            var request = Request("daily/timer/" + dayEntryId);
-
-            if (ofUser != null)
-                request.AddParameter("of_user", ofUser.Value);
+            var request = ToggleDailyRequest(dayEntryId, ofUser);
 
             return Execute<Timer>(request);
+        }
+
+        /// <summary>
+        /// Toggles a single day entry timer by ID. Makes a GET request to the Daily/Timer resource.
+        /// </summary>
+        /// <param name="dayEntryId">The ID of the day entry to retrieve</param>
+        /// <param name="ofUser">The user the day entry belongs to</param>
+        public Task<Timer> ToggleDailyAsync(long dayEntryId, long? ofUser = null)
+        {
+            var request = ToggleDailyRequest(dayEntryId, ofUser);
+
+            return ExecuteAsync<Timer>(request);
         }
 
         /// <summary>
@@ -72,16 +94,25 @@ namespace Harvest.Net
         /// <param name="ofUser">The user the day entry belongs to</param>
         public Timer CreateDaily(DateTime spentAt, long projectId, long taskId, decimal hours, string notes = null, long? ofUser = null)
         {
-            var options = new DailyOptions()
-            {
-                Notes = notes,
-                SpentAt = spentAt,
-                ProjectId = projectId,
-                TaskId = taskId,
-                Hours = hours.ToString("f2")
-            };
+            var options = GetCreateDailyOptions(spentAt, projectId, taskId, hours, notes);
 
             return CreateDaily(options, ofUser);
+        }
+        
+        /// <summary>
+        /// Create a new time entry for the logged in user. Makes a POST request to the Daily/Add resource.
+        /// </summary>
+        /// <param name="spentAt">The date of the entry</param>
+        /// <param name="projectId">The project ID for the entry</param>
+        /// <param name="taskId">The task ID for the entry</param>
+        /// <param name="hours">The hours on the entry</param>
+        /// <param name="notes">The notes on the entry</param>
+        /// <param name="ofUser">The user the day entry belongs to</param>
+        public Task<Timer> CreateDailyAsync(DateTime spentAt, long projectId, long taskId, decimal hours, string notes = null, long? ofUser = null)
+        {
+            var options = GetCreateDailyOptions(spentAt, projectId, taskId, hours, notes);
+
+            return CreateDailyAsync(options, ofUser);
         }
 
         /// <summary>
@@ -96,17 +127,26 @@ namespace Harvest.Net
         /// <param name="ofUser">The user the day entry belongs to</param>
         public Timer CreateDaily(DateTime spentAt, long projectId, long taskId, TimeSpan startedAt, TimeSpan endedAt, string notes = null, long? ofUser = null)
         {
-            var options = new DailyOptions()
-            {
-                Notes = notes,
-                SpentAt = spentAt,
-                ProjectId = projectId,
-                TaskId = taskId,
-                StartedAt = new DateTime(2000, 1, 1).Add(startedAt).ToString("h:mmtt").ToLower(),
-                EndedAt = new DateTime(2000, 1, 1).Add(endedAt).ToString("h:mmtt").ToLower()
-            };
+            var options = GetCreateDailyOptions(spentAt, projectId, taskId, startedAt, endedAt, notes);
 
             return CreateDaily(options, ofUser);
+        }
+
+        /// <summary>
+        /// Create a new time entry for the logged in user. Makes a POST request to the Daily/Add resource.
+        /// </summary>
+        /// <param name="spentAt">The date of the entry</param>
+        /// <param name="projectId">The project ID for the entry</param>
+        /// <param name="taskId">The task ID for the entry</param>
+        /// <param name="startedAt">The start timestamp of the entry</param>
+        /// <param name="endedAt">The end timestamp of the entry</param>
+        /// <param name="notes">The notes on the entry</param>
+        /// <param name="ofUser">The user the day entry belongs to</param>
+        public Task<Timer> CreateDailyAsync(DateTime spentAt, long projectId, long taskId, TimeSpan startedAt, TimeSpan endedAt, string notes = null, long? ofUser = null)
+        {
+            var options = GetCreateDailyOptions(spentAt, projectId, taskId, startedAt, endedAt, notes);
+
+            return CreateDailyAsync(options, ofUser);
         }
 
         /// <summary>
@@ -119,16 +159,24 @@ namespace Harvest.Net
         /// <param name="ofUser">The user the day entry belongs to</param>
         public Timer StartTimer(DateTime spentAt, long projectId, long taskId, string notes = null, long? ofUser = null)
         {
-            var options = new DailyOptions()
-            {
-                Notes = notes,
-                SpentAt = spentAt,
-                ProjectId = projectId,
-                TaskId = taskId,
-                Hours = " "
-            };
+            var options = GetStartTimerOptions(spentAt, projectId, taskId, notes);
 
             return CreateDaily(options, ofUser);
+        }
+
+        /// <summary>
+        /// Starts a new timer for the logged in user. Makes a POST request to the Daily/Add resource.
+        /// </summary>
+        /// <param name="spentAt">The date of the entry</param>
+        /// <param name="projectId">The project ID for the entry</param>
+        /// <param name="taskId">The task ID for the entry</param>
+        /// <param name="notes">The notes on the entry</param>
+        /// <param name="ofUser">The user the day entry belongs to</param>
+        public Task<Timer> StartTimerAsync(DateTime spentAt, long projectId, long taskId, string notes = null, long? ofUser = null)
+        {
+            var options = GetStartTimerOptions(spentAt, projectId, taskId, notes);
+
+            return CreateDailyAsync(options, ofUser);
         }
 
         /// <summary>
@@ -138,11 +186,21 @@ namespace Harvest.Net
         /// <param name="ofUser">The user the day entry belongs to</param>
         internal Timer CreateDaily(DailyOptions options, long? ofUser)
         {
-            var request = Request(ofUser != null ? string.Format("daily/add?of_user={0}", ofUser) : "daily/add", RestSharp.Method.POST);
-
-            request.AddBody(options);
+            var request = CreateDailyRequest(options, ofUser);
 
             return Execute<Timer>(request);
+        }
+
+        /// <summary>
+        /// Create a new time entry for the logged in user. Makes a POST request to the Daily/Add resource.
+        /// </summary>
+        /// <param name="options">The options for the time entry</param>
+        /// <param name="ofUser">The user the day entry belongs to</param>
+        internal Task<Timer> CreateDailyAsync(DailyOptions options, long? ofUser)
+        {
+            var request = CreateDailyRequest(options, ofUser);
+
+            return ExecuteAsync<Timer>(request);
         }
 
         /// <summary>
@@ -152,12 +210,23 @@ namespace Harvest.Net
         /// <param name="ofUser">The user the day entry belongs to</param>
         public bool DeleteDaily(long dayEntryId, long? ofUser = null)
         {
-            var request = Request("daily/delete/" + dayEntryId, RestSharp.Method.DELETE);
-
-            if (ofUser != null)
-                request.AddParameter("of_user", ofUser.Value);
+            var request = DeleteDailyRequest(dayEntryId, ofUser);
 
             var result = Execute(request);
+
+            return result.StatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        /// <summary>
+        /// Delete a day entry for the logged in user. Makes a DELETE request tot he Daily/Delete resource.
+        /// </summary>
+        /// <param name="dayEntryId">The ID of the day entry to delete</param>
+        /// <param name="ofUser">The user the day entry belongs to</param>
+        public async Task<bool> DeleteDailyAsync(long dayEntryId, long? ofUser = null)
+        {
+            var request = DeleteDailyRequest(dayEntryId, ofUser);
+
+            var result = await ExecuteAsync(request).ConfigureAwait(false);
 
             return result.StatusCode == System.Net.HttpStatusCode.OK;
         }
@@ -176,7 +245,160 @@ namespace Harvest.Net
         /// <param name="ofUser">The user the entry belongs to</param>
         public Timer UpdateDaily(long dayEntryId, DateTime? spentAt = null, long? projectId = null, long? taskId = null, decimal? hours = null, TimeSpan? startedAt = null, TimeSpan? endedAt = null, string notes = null, long? ofUser = null)
         {
-            var options = new DailyOptions()
+            var options = GetUpdateDailyOptions(spentAt, projectId, taskId, hours, startedAt, endedAt, notes);
+
+            return UpdateDaily(dayEntryId, options, ofUser);
+        }
+
+        /// <summary>
+        /// Update a time entry for the logged in user. Makes a POST request to the Daily/Update resource.
+        /// </summary>
+        /// <param name="dayEntryId">The ID of the time entry to update</param>
+        /// <param name="spentAt">The new date of the entry</param>
+        /// <param name="projectId">The new project ID of the entry</param>
+        /// <param name="taskId">The new task ID of the entry</param>
+        /// <param name="hours">The new hours for the entry</param>
+        /// <param name="startedAt">The new start timestamp for the entry</param>
+        /// <param name="endedAt">The new end timestamp for the entry</param>
+        /// <param name="notes">The new notes for the entry</param>
+        /// <param name="ofUser">The user the entry belongs to</param>
+        public Task<Timer> UpdateDailyAsync(long dayEntryId, DateTime? spentAt = null, long? projectId = null, long? taskId = null, decimal? hours = null, TimeSpan? startedAt = null, TimeSpan? endedAt = null, string notes = null, long? ofUser = null)
+        {
+            var options = GetUpdateDailyOptions(spentAt, projectId, taskId, hours, startedAt, endedAt, notes);
+
+            return UpdateDailyAsync(dayEntryId, options, ofUser);
+        }
+
+        /// <summary>
+        /// Update a time entry for the logged in user. Makes a POST request to the Daily/Update resource.
+        /// </summary>
+        /// <param name="dayEntryId">The ID of the entry to update</param>
+        /// <param name="options">The options to update</param>
+        /// <param name="ofUser">The user the entry belongs to</param>
+        internal Timer UpdateDaily(long dayEntryId, DailyOptions options, long? ofUser)
+        {
+            var request = Request("daily/update/" + dayEntryId, Method.POST);
+
+            if (ofUser != null)
+                request.AddParameter("of_user", ofUser.Value);
+
+            request.AddBody(options);
+
+            return Execute<Timer>(request);
+        }
+
+        /// <summary>
+        /// Update a time entry for the logged in user. Makes a POST request to the Daily/Update resource.
+        /// </summary>
+        /// <param name="dayEntryId">The ID of the entry to update</param>
+        /// <param name="options">The options to update</param>
+        /// <param name="ofUser">The user the entry belongs to</param>
+        internal Task<Timer> UpdateDailyAsync(long dayEntryId, DailyOptions options, long? ofUser)
+        {
+            var request = Request("daily/update/" + dayEntryId, Method.POST);
+
+            if (ofUser != null)
+                request.AddParameter("of_user", ofUser.Value);
+
+            request.AddBody(options);
+
+            return ExecuteAsync<Timer>(request);
+        }
+
+        private IRestRequest DailyRequest(short? dayOfTheYear, short? year, long? ofUser)
+        {
+            if (year == null && dayOfTheYear != null)
+                throw new ArgumentNullException(nameof(year), "You must provide both dayOfTheYear and year or neither.");
+            if (year != null && dayOfTheYear == null)
+                throw new ArgumentNullException(nameof(dayOfTheYear), "You must provide both dayOfYear and year or neither.");
+
+            var request = Request("daily" + (dayOfTheYear != null ? "/" + dayOfTheYear + "/" + year : string.Empty));
+
+            if (ofUser != null)
+                request.AddParameter("of_user", ofUser.Value);
+
+            return request;
+        }
+
+        private IRestRequest DailyRequest(long dayEntryId, long? ofUser)
+        {
+            var request = Request("daily/show/" + dayEntryId);
+
+            if (ofUser != null)
+                request.AddParameter("of_user", ofUser.Value);
+
+            return request;
+        }
+
+        private IRestRequest ToggleDailyRequest(long dayEntryId, long? ofUser)
+        {
+            var request = Request("daily/timer/" + dayEntryId);
+
+            if (ofUser != null)
+                request.AddParameter("of_user", ofUser.Value);
+
+            return request;
+        }
+
+        private static DailyOptions GetCreateDailyOptions(DateTime spentAt, long projectId, long taskId, decimal hours, string notes)
+        {
+            return new DailyOptions
+            {
+                Notes = notes,
+                SpentAt = spentAt,
+                ProjectId = projectId,
+                TaskId = taskId,
+                Hours = hours.ToString("f2")
+            };
+        }
+
+        private static DailyOptions GetCreateDailyOptions(DateTime spentAt, long projectId, long taskId, TimeSpan startedAt, TimeSpan endedAt, string notes)
+        {
+            return new DailyOptions
+            {
+                Notes = notes,
+                SpentAt = spentAt,
+                ProjectId = projectId,
+                TaskId = taskId,
+                StartedAt = new DateTime(2000, 1, 1).Add(startedAt).ToString("h:mmtt").ToLower(),
+                EndedAt = new DateTime(2000, 1, 1).Add(endedAt).ToString("h:mmtt").ToLower()
+            };
+        }
+
+        private static DailyOptions GetStartTimerOptions(DateTime spentAt, long projectId, long taskId, string notes)
+        {
+            return new DailyOptions
+            {
+                Notes = notes,
+                SpentAt = spentAt,
+                ProjectId = projectId,
+                TaskId = taskId,
+                Hours = " "
+            };
+        }
+
+        private IRestRequest CreateDailyRequest(DailyOptions options, long? ofUser)
+        {
+            var request = Request(ofUser != null ? $"daily/add?of_user={ofUser}" : "daily/add", Method.POST);
+
+            request.AddBody(options);
+
+            return request;
+        }
+
+        private IRestRequest DeleteDailyRequest(long dayEntryId, long? ofUser)
+        {
+            var request = Request("daily/delete/" + dayEntryId, Method.DELETE);
+
+            if (ofUser != null)
+                request.AddParameter("of_user", ofUser.Value);
+
+            return request;
+        }
+
+        private static DailyOptions GetUpdateDailyOptions(DateTime? spentAt, long? projectId, long? taskId, decimal? hours, TimeSpan? startedAt, TimeSpan? endedAt, string notes)
+        {
+            var options = new DailyOptions
             {
                 SpentAt = spentAt,
                 ProjectId = projectId,
@@ -193,25 +415,7 @@ namespace Harvest.Net
             if (endedAt != null)
                 options.EndedAt = new DateTime(2000, 1, 1).Add(endedAt.Value).ToString("h:mmtt").ToLower();
 
-            return UpdateDaily(dayEntryId, options, ofUser);
-        }
-
-        /// <summary>
-        /// Update a time entry for the logged in user. Makes a POST request to the Daily/Update resource.
-        /// </summary>
-        /// <param name="dayEntryId">The ID of the entry to update</param>
-        /// <param name="options">The options to update</param>
-        /// <param name="ofUser">The user the entry belongs to</param>
-        internal Timer UpdateDaily(long dayEntryId, DailyOptions options, long? ofUser)
-        {
-            var request = Request("daily/update/" + dayEntryId, RestSharp.Method.POST);
-
-            if (ofUser != null)
-                request.AddParameter("of_user", ofUser.Value);
-
-            request.AddBody(options);
-
-            return Execute<Timer>(request);
+            return options;
         }
     }
 }
